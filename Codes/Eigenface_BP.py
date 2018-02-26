27c197a628026e2917a55303c877f3a6e83db141
Vergil Zhu <zhuzc2333@gmail.com>
2018-02-26 05:43:39 -0800

    updated to Matrix A

--- Codes/Eigenface_BP.py
+++ Codes/Eigenface_BP.py
@@ -34,13 +34,15 @@ num_test_images = int(num_samples * num_test)
 
 imgSet_train = []
 imgSet_test = []
+labSet_train = []
+labSet_test = []
 imSize = [112, 92]
 
 
 with tf.Session() as sess:
 
 
-
+	# Build image set for train
 	for i in range(1, num_train+1):
 		for j in range(1, num_samples+1):
 
@@ -48,12 +50,16 @@ with tf.Session() as sess:
 			image_raw_data_jpg = tf.gfile.FastGFile(imgPath, 'r').read()
 			img_data_jpg = tf.image.decode_jpeg(image_raw_data_jpg)
 			img_data_jpg = tf.image.convert_image_dtype(img_data_jpg, dtype=tf.uint8)
-			img_data_jpg = tf.cast(img_data_jpg, tf.int16)
-			# dtype is changed to tf.int16 instead of tf.uint8 cause not supported on this computer
-			img_data_jpg = tf.reshape(img_data_jpg, [1, imSize[0]*imSize[1]])
+			img_data_jpg = tf.cast(img_data_jpg, tf.int32)
+			# dtype is changed to tf.int32 instead of tf.uint8 cause not supported on this computer
+			img_data_jpg = tf.reshape(img_data_jpg, [imSize[0]*imSize[1]])
 			
 			imgSet_train.append(img_data_jpg)
 
+			labSet_train.append(j)
+
+
+	# Build image set & label for test
 	for i in range(num_train+1, num_images_each_sample+1):
 		for j in range(1, num_samples+1):
 
@@ -61,20 +67,38 @@ with tf.Session() as sess:
 			image_raw_data_jpg = tf.gfile.FastGFile(imgPath, 'r').read()
 			img_data_jpg = tf.image.decode_jpeg(image_raw_data_jpg)
 			img_data_jpg = tf.image.convert_image_dtype(img_data_jpg, dtype=tf.uint8)
-			img_data_jpg = tf.cast(img_data_jpg, tf.int16)
-			# dtype is changed to tf.int16 instead of tf.uint8 cause not supported on this computer
-			img_data_jpg = tf.reshape(img_data_jpg, [1, imSize[0]*imSize[1]])
+			img_data_jpg = tf.cast(img_data_jpg, tf.int32)
+			# dtype is changed to tf.int32 instead of tf.uint8 cause not supported on this computer
+			img_data_jpg = tf.reshape(img_data_jpg, [imSize[0]*imSize[1]])
 			
 			imgSet_test.append(img_data_jpg)
 
+			labSet_test.append(j) # label array for test
 
+	# Calculate mean image
+	imgSet_mean = tf.add_n(imgSet_train)/num_train_images
+	#print(imgSet_mean.eval())
 
+	mean_face = tf.reshape(imgSet_mean, [112, 92])
 
-	temp = tf.add_n(imgSet_train)/num_train_images
-	print(temp.eval())
+	# plt.figure('Face1')
+	# plt.imshow(mean_face.eval(), cmap=plt.cm.gray_r)
+	# plt.show()
 
-	mean_face = tf.reshape(temp, [112, 92])
+	# Use EIGENFACE method
+	# Determine the difference matrix: A 
+	A_temp = imgSet_train - np.tile(imgSet_mean, num_train_images)
 
-	plt.figure('Face1')
-	plt.imshow(mean_face.eval(), cmap=plt.cm.gray_r)
-	plt.show()
+	# The following steps solve the problem of tf.stack(A_temp,0)
+	A = []
+	for i in range(0, num_train_images):
+		A.append(A_temp[i])
+
+	A = tf.stack(A,0)
+	print(A.eval())
+	A = tf.transpose(A)
+
+	print(A.eval())
+	#print(tf.stack(A_temp, 0))
+	# Obtain the eigenvectors & eigenvalues of A
+	#print(imgSet_mean.eval())
